cee0717f2c80309af638a76dcd2b9e7aa7b0d430
thanhhht1704 <thanh.ho@tomota.vn>
2025-05-31 22:34:22 +0700

    chore: remove unnecessary file

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import { AppDataSource } from '@/config/data-source.js';
 AppDataSource.initialize()
   .then(() => {
     app.listen(config.port, () => {
-      console.log(`🚀Server running on port:${config.port}`);
+      console.log(`🚀 Server running on port:${config.port}`);
     });
   })
   .catch((error) => console.log(error));
